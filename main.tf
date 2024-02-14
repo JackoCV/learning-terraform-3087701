d2fb7c6b5fe97b6b869a9b03b05b3a5b264d8c7a
Jacksem Cortes <90305097+JackoCV@users.noreply.github.com>
2024-02-14 14:01:50 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_sercurity_group_ids = [aws_security_group.blog.id]
+  vpc_security_group_ids = [aws_security_group.blog.id]
   tags = {
     Name = "HelloWorld"
   }
@@ -35,7 +35,7 @@ resource "aws_security_group" "blog" {
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
-  type      = " ingress"
+  type        = " ingress"
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
@@ -44,7 +44,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 resource "aws_security_group_rule" "blog_https_in" {
-  type      = " ingress"
+  type        = " ingress"
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
@@ -54,7 +54,7 @@ resource "aws_security_group_rule" "blog_https_in" {
 } 
 
 resource "aws_security_group_rule" "blog_everything_out" {
-  type      = " egress"
+  type        = " egress"
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
